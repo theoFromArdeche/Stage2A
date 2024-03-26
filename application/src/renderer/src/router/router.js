9c4c8d8da941ae4ed0b4282ef8faab422e935f7e
theo <theodeygas@gmail.com>
2024-03-26 15:24:46 +0100

    modif router

--- application/src/renderer/src/router/router.js
+++ application/src/renderer/src/router/router.js
@@ -6,10 +6,10 @@ import Parametres from '../vues/Parametres.vue'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: Simulation }, // Route de base vers le live
+    { path: '/', component: Simulation }, // Route de base vers la simulation
     { path: '/live', component: Live }, // Route pour le live
     { path: '/simulation', component: Simulation }, // Route pour la simulation
-    { path: '/parametres', component: Parametres } // Route pour les paramêtres
+    { path: '/parametres', component: Parametres } // Route pour les paramètres
   ]
 })
 
