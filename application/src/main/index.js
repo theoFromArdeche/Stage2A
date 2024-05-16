46e2bf6f6fdb8895d1a3582c08e5e3e5b66ed3ea
theo <theodeygas@gmail.com>
2024-05-16 15:38:53 +0200

    roadmap requetes

--- application/src/main/index.js
+++ application/src/main/index.js
@@ -95,7 +95,7 @@ const server = net.createServer((socket) => {
   console.log('Client connected');
   clientSocket=socket;
   clientConnected=true
-
+  console.log('Client socket info:', socket.address());
   // received data from the client
   socket.on('data', (data) => {
     receiveRequest(data.toString())
@@ -104,7 +104,15 @@ const server = net.createServer((socket) => {
   socket.on('end', () => {
     console.log('Client disconnected');
     clientConnected=false
+    console.log('Client socket info:', socket.address());
   });
+
+  // Event handler for errors
+  socket.on('error', (err) => {
+    console.error('Socket error:', err);
+    clientConnected = false;
+  });
+
 });
 
 server.listen(port_server, () => {
@@ -123,3 +131,98 @@ function receiveRequest(msg) {
   console.log('Received from client : ', msg);
   mainWindow.webContents.send('receiveResponse', msg);
 }
+
+
+
+
+
+
+/*
+
+
+
+initialisation de l'instance: 
+ping le server (pour qu'il puisse nous envoyer les majs)
+fetch les données (matrice des temps et des réussites)
+
+
+
+
+
+etudiant -> instance
+
+
+
+
+live:
+l'étudiant demande la main (via l'instance (on appuie sur le boutton demander la main))
+l'instance demande au server et recup la réponse
+
+
+
+
+live: (on pars du principe qu'on a la main pour le serveur)
+transmettre la requete au server
+instance -> server :
+
+récup la réponse (toutes les réponses du robot)
+récup donné par le server : temps, réusite
+
+on met a jour les infos locales (matrice des temps, de réussites)
+mettre a jour le visu
+
+on envoie les réponses a l'étudiant
+
+
+
+
+live: (on pars du principe qu'on a PAS la main pour le serveur)
+on notifie l'étudiant qu'il n'a pas la main et on lui donne sa position dans la file d'attente
+
+
+
+simulation:
+renvoyer la reponse que le robot aurait renvoyé
+mettre a jour le visu (temps + réussite avec une ligne entre le départ et l'arrivée)
+(a voir si on a fait une maj du visu instantanée ou genre 2sec pour une animation)
+
+
+
+
+
+
+
+
+server:
+
+
+reçois un ping, enregistre l'instance
+
+ping l'instance toutes les x secondes pour voir si elle est encore la
+si timeout -> il supprime l'instance
+
+
+reçois une requete
+
+
+si pas dispo, ajoute a la file d'attente
+(potentiellement notifie l'instance)
+
+
+si il est dispo:
+envoyer la requete au robot
+recup la reponse
+mettre a jour les données (matrice des temps et des réussites)
+envoyer la réponse à l'instance qui a fait la requete
+envoyer les modifs a tous le monde (temps + réussite)
+
+
+
+
+une instance demande la main: 
+on l'ajoute a la file d'attente (si déjà prise)
+
+il y a un timeout quand on donne la main à une instance
+
+
+*/
