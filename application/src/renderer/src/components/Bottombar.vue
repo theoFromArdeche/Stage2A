aafd4f8233b7b3ee3815ddde11d6ae3679e0867d
theo <theodeygas@gmail.com>
2024-05-27 16:56:01 +0200

    update scrollbar

--- application/src/renderer/src/components/Bottombar.vue
+++ application/src/renderer/src/components/Bottombar.vue
@@ -1,16 +1,30 @@
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue'
+import { ref, onMounted, onBeforeUnmount, nextTick, watch } from 'vue'
 
 //const emit = defineEmits(['send-request'])
 
 const statusMessage = ref([])
+const container_statusMessages = ref(null)
 
 const ipcRenderer = window.electron.ipcRenderer
 
 ipcRenderer.on('updateStatus', (event, arg) => {
+  const flagScroll = container_statusMessages.value.scrollHeight === container_statusMessages.value.offsetHeight + container_statusMessages.value.scrollTop;
+  const oldScrollTop = container_statusMessages.value.scrollTop
   statusMessage.value.unshift(arg)
-  console.log(statusMessage)
+  updateScrollbar(flagScroll, oldScrollTop);
+  //console.log(statusMessage)
 })
+
+
+async function updateScrollbar(flagScroll, oldScrollTop) {
+  await nextTick();
+  if (flagScroll) {
+    container_statusMessages.value.scrollTop = container_statusMessages.value.scrollHeight;
+  } else {
+    container_statusMessages.value.scrollTop = oldScrollTop
+  }
+};
 
 const containerHeight = ref('100px')
 const maxHeight = window.innerHeight * 0.8
@@ -55,7 +69,7 @@ onBeforeUnmount(() => {
   <div>
     <div id="resize_handle" @mousedown="startResizing"></div>
     <div :style="{ height: containerHeight }" id="container_bar">
-      <div id="container_statusMessages">
+      <div ref="container_statusMessages" id="container_statusMessages">
         <div id="scrollbar">
           <div v-for="(message, index) in statusMessage" :key="index">{{ message }}<br /></div>
         </div>
